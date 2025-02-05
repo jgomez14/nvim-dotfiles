0fcdcf332fd63516b0cfc0bf8b237ae73eaffd1e
Javier Gómez Resco <javier.gomezresco@gmail.com>
2025-02-05 23:11:03 +0100

    feat: lazy.nvim added, catppuccin added
    lazy.nvim configured following the latest setup instructions for
    declaring each plugin in its own file.
    
    catppuccin colorscheme installed and setup.
    
    basic options configured: number, relativenumber, tab as 4 spaces...

---
+++ lua/config/options.lua
@@ -0,0 +1,23 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.cursorline = true
+
+vim.opt.hlsearch = false
+
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 10
+
+-- Set tab behaviour
+
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 0
+
+-- Set coloscheme
+
+vim.cmd.colorscheme("catppuccin")
