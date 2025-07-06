2d5512f2008c3cd58eb97de61472aa59c1d40c99
Javier Gómez Resco <javier.gomezresco@gmail.com>
2025-07-06 13:38:29 +0200

    feat: install telescope

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.cmd("filetype plugin indent on")
+vim.cmd.filetype("plugin indent on")
 
 vim.opt.number = true
 vim.opt.relativenumber = true
