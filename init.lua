8fa2c88608171205ae85a034cedc7fa8b1a8e2b8
Javier Gómez Resco <javier.gomezresco@gmail.com>
2025-07-06 13:08:34 +0200

    feat: lazy plugin manager and colors

--- init.lua
+++ init.lua
@@ -1,6 +1,15 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.cmd("filetype plugin indent on")
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = -1
 vim.opt.expandtab = true
+
+vim.keymap.set("n", "<leader>-", ":Ex<CR>")
+
+require("config.lazy")
---
+++ lua/plugins/vague.lua
@@ -0,0 +1,1 @@
+return { "vague2k/vague.nvim", opts = {}, init = function() vim.cmd.colorscheme("vague") end }
