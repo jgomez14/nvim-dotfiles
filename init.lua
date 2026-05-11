a8a54f6bf96ba5b6ccd778ec65ef3a92d61c11a1
Javier Gómez Resco <javier.gomezresco@gmail.com>
2026-05-11 20:58:04 +0200

    feat: improve autocompletion usage with tab and enter

--- init.lua
+++ init.lua
@@ -71,12 +71,25 @@ vim.opt.complete = {
   "u",
   "t",
 }
+
 vim.opt.completeopt = {
   "menuone",
   -- Very important
   "noselect",
   "preview"
 }
+
+vim.keymap.set("i", "<Tab>", function ()
+  return vim.fn.pumvisible() == 1 and "<C-n>" or "<Tab>"
+end, { expr = true })
+
+vim.keymap.set("i", "<S-Tab>", function ()
+  return vim.fn.pumvisible() == 1 and "<C-p>" or "<S-Tab>"
+end, { expr = true })
+
+vim.keymap.set("i", "<Enter>", function ()
+  return vim.fn.pumvisible() == 1 and "<C-y>" or "<Enter>"
+end, { expr = true })
 
 -- Filetypes
 vim.filetype.add({
